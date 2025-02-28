b288ea367b298c0a2f1a93bbffbd1acfcbeda3b8
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-02-28 22:52:11 +0300

    Update SubCategories

--- MarketWorld.Domain/Entities/SubCategories.cs
+++ MarketWorld.Domain/Entities/SubCategories.cs
@@ -1,6 +1,7 @@
 using MarketWorld.Domain.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MarketWorld.Domain.Entities
 {
@@ -10,8 +11,10 @@ namespace MarketWorld.Domain.Entities
         
         public string Description { get; set; }
         
+        
         public long CategoryId { get; set; }
         
+        [ForeignKey("CategoryId")]
         public Categories Categories { get; set; }
         
         public ICollection<Products>? Products { get; set; }
