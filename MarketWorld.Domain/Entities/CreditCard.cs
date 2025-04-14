02aa1a5834630691b3a75e5ee9d966795069ad67
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-04-14 15:21:09 +0300

    CreditCard Update

--- MarketWorld.Domain/Entities/CreditCard.cs
+++ MarketWorld.Domain/Entities/CreditCard.cs
@@ -6,8 +6,9 @@ namespace MarketWorld.Domain.Entities
     public class CreditCard : BaseEntity
     {
         public string CardHolderName { get; set; }
-        public string MaskedCardNumber { get; set; }
+        public string CardNumber { get; set; }
         public string ExpiryDate { get; set; }
+        public string Cvv { get; set; }
         public string CardType { get; set; }
         public bool IsDefault { get; set; }
         
