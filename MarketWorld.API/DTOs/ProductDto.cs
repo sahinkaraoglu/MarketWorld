c265f5809276c07e947b4061baf640671630c6f9
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-05-13 16:43:24 +0300

    Revert "CacheController Added"
    This reverts commit de26d8cc0f7b3882c471341007e6971ea5c59e19.

--- MarketWorld.API/DTOs/ProductDto.cs
+++ MarketWorld.API/DTOs/ProductDto.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace MarketWorld.API.DTOs
 {
@@ -9,7 +8,6 @@ namespace MarketWorld.API.DTOs
         public int ProductCode { get; set; }
         public string Name { get; set; }
         public int BrandId { get; set; }
-        public string BrandName { get; set; }
         public decimal Price { get; set; }
         public bool IsActive { get; set; } = true;
         public string Description { get; set; }
@@ -17,19 +15,6 @@ namespace MarketWorld.API.DTOs
         public decimal Rating { get; set; }
         public bool HasDiscount { get; set; } = false;
         public int? CategoryId { get; set; }
-        public string CategoryName { get; set; }
         public int? SubCategoryId { get; set; }
-        public string SubCategoryName { get; set; }
-        public List<string> ImageUrls { get; set; }
-        public List<ProductPropertyDto> ProductProperties { get; set; }
-    }
-
-    public class ProductPropertyDto
-    {
-        public int Id { get; set; }
-        public string PropertyTypeName { get; set; }
-        public string PropertyValue { get; set; }
-        public int Stock { get; set; }
-        public bool IsActive { get; set; }
     }
 } 
