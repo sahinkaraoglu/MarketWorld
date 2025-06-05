201c41e540d58b5d4a384f1dc28ac3c22d0c9c42
Şahin Karaoğlu <76259114+sahinkaraoglu@users.noreply.github.com>
2025-06-06 00:11:54 +0300

    Update PanelController.cs

--- MarketWorld.Web/Areas/Admin/Controllers/PanelController.cs
+++ MarketWorld.Web/Areas/Admin/Controllers/PanelController.cs
@@ -50,70 +50,84 @@ namespace MarketWorld.Web.Areas.Admin.Controllers
         [Route("Index")]
         public async Task<IActionResult> Index()
         {
-            string cacheKey = "admin_panel_dashboard";
-            string cachedData = await _cache.GetStringAsync(cacheKey);
-
-            if (!string.IsNullOrEmpty(cachedData))
+            try
             {
-                var cachedDashboardData = JsonSerializer.Deserialize<Dictionary<string, int>>(cachedData, _jsonOptions);
-                foreach (var item in cachedDashboardData)
+                string cacheKey = "admin_panel_dashboard";
+                string cachedData = await _cache.GetStringAsync(cacheKey);
+
+                if (!string.IsNullOrEmpty(cachedData))
                 {
-                    ViewBag[item.Key] = item.Value;
+                    var cachedDashboardData = JsonSerializer.Deserialize<Dictionary<string, int>>(cachedData, _jsonOptions);
+                    foreach (var item in cachedDashboardData)
+                    {
+                        ViewBag.SetValue(item.Key, item.Value);
+                    }
+                    return View();
                 }
+
+                // Sıralı işlem yapısı
+                var products = await _productService.GetAllProducts() ?? new List<Product>();
+                var categories = await _categoryService.GetAllCategoriesAsync() ?? new List<Category>();
+                var subCategories = await _subCategoryService.GetAllSubCategoriesAsync() ?? new List<SubCategory>();
+                var users = await _userManager.Users.ToListAsync() ?? new List<ApplicationUser>();
+                var brands = await _brandService.GetAllBrandsAsync() ?? new List<Brand>();
+                var topSellerBrands = await _orderService.GetTopSellingBrandsAsync(10) ?? new List<Brand>();
+                
+                // Ürün istatistikleri
+                ViewBag.ProductsCount = products.Count();
+                ViewBag.LowStockCount = products.Count(p => p.GetTotalStock() < 10);
+                    
+                // Kategori istatistikleri
+                ViewBag.CategoriesCount = categories.Count();
+                ViewBag.SubCategoriesCount = subCategories.Count();
+                
+                // Kullanıcı istatistikleri
+                ViewBag.TotalUsersCount = users.Count;
+                ViewBag.NewUsersCount = users.Count(u => u.CreateDate >= DateTime.Now.AddDays(-7));
+                    
+                // Marka istatistikleri
+                ViewBag.BrandsCount = brands.Count();
+                ViewBag.TopSellerBrandsCount = topSellerBrands.Count();
+                
+                // Önbelleğe kaydet
+                var dashboardDataToCache = new Dictionary<string, int>
+                {
+                    { "ProductsCount", ViewBag.ProductsCount },
+                    { "LowStockCount", ViewBag.LowStockCount },
+                    { "CategoriesCount", ViewBag.CategoriesCount },
+                    { "SubCategoriesCount", ViewBag.SubCategoriesCount },
+                    { "TotalUsersCount", ViewBag.TotalUsersCount },
+                    { "NewUsersCount", ViewBag.NewUsersCount },
+                    { "BrandsCount", ViewBag.BrandsCount },
+                    { "TopSellerBrandsCount", ViewBag.TopSellerBrandsCount }
+                };
+
+                var cacheOptions = new DistributedCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(15))
+                    .SetAbsoluteExpiration(TimeSpan.FromHours(2));
+
+                await _cache.SetStringAsync(
+                    cacheKey,
+                    JsonSerializer.Serialize(dashboardDataToCache, _jsonOptions),
+                    cacheOptions
+                );
+                
                 return View();
             }
-
-            var products = await _productService.GetAllProducts();
-            
-            // Ürün istatistikleri
-            ViewBag.ProductsCount = products.Count();
-            
-            // Stok az olan ürün sayısını hesaplama
-            ViewBag.LowStockCount = products.Count(p => p.GetTotalStock() < 10);
-                
-            // Kategori istatistikleri
-            var categories = await _categoryService.GetAllCategoriesAsync();
-            var subCategories = await _subCategoryService.GetAllSubCategoriesAsync();
-            ViewBag.CategoriesCount = categories.Count();
-            ViewBag.SubCategoriesCount = subCategories.Count();
-            
-            // Kullanıcı istatistikleri
-            var users = await _userManager.Users.ToListAsync();
-            ViewBag.TotalUsersCount = users.Count;
-            ViewBag.NewUsersCount = users.Count(u => u.CreateDate >= DateTime.Now.AddDays(-7));
-                
-            // Marka istatistikleri
-            var brands = await _brandService.GetAllBrandsAsync();
-            ViewBag.BrandsCount = brands.Count();
-            
-            // Çok satan markaları hesapla
-            var topSellerBrands = await _orderService.GetTopSellingBrandsAsync(10);
-            ViewBag.TopSellerBrandsCount = topSellerBrands.Count();
-            
-            // Önbelleğe kaydet
-            var dashboardDataToCache = new Dictionary<string, int>
+            catch (Exception ex)
             {
-                { "ProductsCount", ViewBag.ProductsCount },
-                { "LowStockCount", ViewBag.LowStockCount },
-                { "CategoriesCount", ViewBag.CategoriesCount },
-                { "SubCategoriesCount", ViewBag.SubCategoriesCount },
-                { "TotalUsersCount", ViewBag.TotalUsersCount },
-                { "NewUsersCount", ViewBag.NewUsersCount },
-                { "BrandsCount", ViewBag.BrandsCount },
-                { "TopSellerBrandsCount", ViewBag.TopSellerBrandsCount }
-            };
-
-            var cacheOptions = new DistributedCacheEntryOptions()
-                .SetSlidingExpiration(TimeSpan.FromMinutes(5))
-                .SetAbsoluteExpiration(TimeSpan.FromHours(1));
-
-            await _cache.SetStringAsync(
-                cacheKey,
-                JsonSerializer.Serialize(dashboardDataToCache, _jsonOptions),
-                cacheOptions
-            );
-            
-            return View();
+                // Hata durumunda varsayılan değerler
+                ViewBag.ProductsCount = 0;
+                ViewBag.LowStockCount = 0;
+                ViewBag.CategoriesCount = 0;
+                ViewBag.SubCategoriesCount = 0;
+                ViewBag.TotalUsersCount = 0;
+                ViewBag.NewUsersCount = 0;
+                ViewBag.BrandsCount = 0;
+                ViewBag.TopSellerBrandsCount = 0;
+                
+                return View();
+            }
         }
 
         [HttpGet]
